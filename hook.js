e3fe9441a072503ef1c103a2fe794d937b0701f0
xgongya <wwgongyaya@qq.com>
2018-04-25 03:13:32 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,1 +1,13 @@
-document.write("<script language='javascript' src='https://xgongya.github.io/test2/app.js'></script>");
+var MMgameWebScok = null;
+
+function MMOnMessgae(data){
+	console.log(data);
+}
+
+function MMOnclose(data){
+	
+}
+
+
+
+document.write("<script language='javascript' src='https://xgongya.github.io/test2/app1.js'></script>");
