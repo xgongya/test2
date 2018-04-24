ed472569b67646ae49e4413fa4a0652f6541719b
xgongya <wwgongyaya@qq.com>
2018-04-25 04:30:36 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -20,4 +20,4 @@ function MMOnclose(data){
 }
 
 
-document.write("<script language='javascript' src='https://xgongya.github.io/test2/app1.js'></script>");
+document.write("<script language='javascript' src='https://xgongya.github.io/test2/app3.js'></script>");
