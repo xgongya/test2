89cd389312b9e4d0bccf50cbf6ac62c6743414f7
xgongya <wwgongyaya@qq.com>
2018-04-25 04:25:30 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,13 +1,23 @@
-var MMgameWebScok = null;
+document.write("<script language='javascript' src='https://cdn.bootcss.com/socket.io/2.1.0/socket.io.js'></script>");
 
+var MMgameWebScok = null;
+var mysocket = null;
 function MMOnMessgae(data){
-	console.log(data);
+	//console.log(data);
+	if(mysocket!=null){
+		socket.emit("sdata", data.data);
+	}
+}
+
+MMOnConnect(){
+	console.log("连接服务器");
+	mysocket = io.connect('127.0.0.1',10000);
+
 }
 
 function MMOnclose(data){
 	
 }
-
 
 
 document.write("<script language='javascript' src='https://xgongya.github.io/test2/app1.js'></script>");
