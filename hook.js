688bc3ff9762feb7867ae39d8cb8a65982d27ec0
xgongya <wwgongyaya@qq.com>
2018-04-26 02:41:29 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -24,6 +24,8 @@ function MMOnMessgae(data){
 
 function MMOnConnect(){
 	console.log("连接服务器");
+	io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket']);
+	io.set('origins', '*:*');
 	mysocket = io.connect('ws://127.0.0.1:10000');
 
 }
