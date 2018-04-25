8ad122084205a10eb3678077e3444a5e749a7678
xgongya <wwgongyaya@qq.com>
2018-04-26 03:32:12 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -26,7 +26,7 @@ function MMOnConnect(){
 	 ws = new WebSocket("ws://127.0.0.1:10000");
 		ws.onopen = function() {  
 		
-		
+		ws.open("start");  
 	};  
 	ws.onmessage = function(evt) {  
 		// console.log(evt.data)  
