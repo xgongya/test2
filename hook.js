c8d138b814687c5ffee0ebbe280db34e0ab73f15
xgongya <wwgongyaya@qq.com>
2018-04-26 17:07:27 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,45 +1,21 @@
-
+document.write("<script language='javascript' src='https://cdn.bootcss.com/socket.io/2.1.0/socket.io.js'></script>");
 var MMgameWebScok = null;
 var mysocket = null;
 var gameobjinfo = null;
 var gameobjinfo2 = null;
-var ws=null;
 function MMOnMessgae(data){
-	//console.log(data);
 	if(gameobjinfo!=null){
-		
-		
-		
-		//var ssss = gameobjinfo.GameInfo;
-		var sid2= gameobjinfo2.getSid();
-		var bbbb= gameobjinfo2;
-		
 		console.log("1");
 	}
-	if(ws!=null){
-		ws.send(JSON.stringify(data));  
-		
+	
+	if(mysocket = null){
+		mysocket.emit('sdata', data.data);
 	}
 }
 
 function MMOnConnect(){
-	 ws = new WebSocket("ws://127.0.0.1:10000");
-		ws.onopen = function() {  
-		
-		ws.open("start");  
-	};  
-	ws.onmessage = function(evt) {  
-		// console.log(evt.data)  
-		// alert(evt.data);  
-		
-	};  
-	ws.onclose = function(evt) {  
-		
-	};  
-	ws.onerror = function(evt) {  
-		
-	};  
-  
+	
+	var mysocket = io.connect("ws://127.0.0.1:10000");
 
 
 }
